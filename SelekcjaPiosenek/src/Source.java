34380cdef6d9b4ebfb5eda89cd2b8eaca227d61c
Krystian Jachna <krysti4nape@gmail.com>
2022-05-18 00:04:37 +0200

    Zrobione komentarze pierwsze wrzucenie BaCa

--- SelekcjaPiosenek/src/Source.java
+++ SelekcjaPiosenek/src/Source.java
@@ -1,6 +1,6 @@
 // Krystian Jachna - 7
 import java.util.Scanner;
-// todo komentarze
+
 
 public class Source {
     public static Scanner sc = new Scanner(System.in);
@@ -59,10 +59,15 @@ public class Source {
             else
                 l = pivIndex + 1;
         }
+        /*
+            Select wywoluje funkcje szukajaca pivota, ktory dzieli array na piatki,
+            W piatkach obliczane sa mediany, ktore nastepnie ustawiane sa na pocz-
+            atku array.
+         */
     }
 
     public static int getPivotIndex(int l, int r) {
-        if (r - l + 1 <= 5)          // dla 5 lub mniej elementow mediana
+        if (r - l + 1 <= 5)                             // dla 5 lub mniej elementow mediana
             return sortAndMedian5(l, r);
 
         for (int i = l; i <= r; i+=5) {
@@ -78,22 +83,11 @@ public class Source {
         }
         int mid = (r - l) / 10 + l + 1;
         return select(l, l + ((r - l)/5), mid);
+        /*
+            Funkcja tworzy podtablice po 5 elementow, a nastepnie liczy mediane
+            kazdej z tych podtablic.
+         */
     }
-
-    /*
-    public static int sortAndMedian5(int l, int r) {
-        for (int i = l + 1; i <= r; ++i) {
-            int tmp = tab[i];
-            int j = i - 1;
-            while (j >= l && tmp < tab[j]) {
-                tab[j+1] = tab[j];
-                --j;
-            }
-            tab[j+1] = tmp;
-        }
-        return (l + r) / 2;
-    }
-    */
 
     public static int sortAndMedian5(int l, int r) {
         boolean swapped = false;
@@ -112,23 +106,28 @@ public class Source {
             swapped = false;
         }
         return (l + r) / 2;
+        /*
+            Bubble sort dla pieciu lub mniej elementow. Z dodatkowa
+            flaga, aby sprawdzac czy zostala wykonana jakas zamiana
+            Funkcja zwraca indeks mediany podtablicy.
+         */
     }
 
 
     public static int partition(int l, int r, int k, int pivIndex) {
-        int pivValue = tab[pivIndex];
-        // pivot na koniec
-        int tmp = tab[pivIndex];
+        int pivValue = tab[pivIndex];       // wartosc pivota
+
+        int tmp = tab[pivIndex];            // pivot na koniec
         tab[pivIndex] = tab[r];
         tab[r] = tmp;
         int idexEqualElems;
 
-        int i = l - 1;
+        int i = l - 1;                      // do algorytmu Hora
         int j = r;
 
         while(true) {
-            while(tab[++i] < pivValue);
-            while(j > l && tab[--j] >= pivValue);
+            while(tab[++i] < pivValue);                  // szuka pierwszego z lewej wiekszego od pivota elementu
+            while(j > l && tab[--j] >= pivValue);        // szuka pierwszego z prawej mniejszego elementu od pivota
 
             if (i >= j)
                 break;
@@ -138,13 +137,14 @@ public class Source {
                 tab[j] = tmp;
             }
         }
-        tmp = tab[i];
+        tmp = tab[i];                                   // wstawia pivota na odpowiednie miejsce
         tab[i] = tab[r];
         tab[r] = tmp;
-        int indexSmallerElems = i;
-        idexEqualElems = i;
+
+        int indexSmallerElems = i;                      // indeks pivota
+        idexEqualElems = i;                             // indeks elementow konca elementow rownych pivotowi
         ++i;
-        for (j = i; j <= r; ++j) {
+        for (j = i; j <= r; ++j) {                      // przenosi za pivota elementy rowne mu
             if (tab[j] == pivValue) {
                  tmp = tab[j];
                  tab[j] = tab[i];
@@ -154,14 +154,28 @@ public class Source {
             }
         }
 
-        if (k < indexSmallerElems)
+        if (k < indexSmallerElems)                      // w zaleznosci w ktorej czesci jest nasze k zwraca odpowiedni element
             return indexSmallerElems;
         if (k <= idexEqualElems)
             return k;
         return idexEqualElems;
+
+        /*
+            Funkcja wyznacza pivot podtablicy i podobnie jak w QuickSort
+            ustawia elementy mniejsze od pivota na lewo od niego, wieksz
+            na prawo z ta roznica ze rowne mu zostaja na miejsciu za pi-
+            votem.
+         */
     }
 }
 /*
+TEST.IN
+1
+100
+-5 7 19 18 10 20 5 17 19 14 -1 15 -9 -12 15 -14 4 -11 -18 19 -8 15 6 11 -17 4 -13 -11 -1 19 15 3 13 -13 -20 4 -1 4 13 -9 -9 16 4 1 7 -3 9 12 -14 18 7 -17 11 1 -15 -12 15 13 12 -12 13 -20 19 1 -2 9 -20 1 -16 20 4 9 -18 -6 -12 -6 16 -20 8 -8 -15 -13 15 -2 18 -2 0 -11 8 11 -4 -9 2 -11 9 7 8 -5 17 20
+5
+13 30 78 88 100
+
 3
 5
 1 2 3 4 5
@@ -176,7 +190,22 @@ public class Source {
 5
 1 10 0 20 11
 
-+++
+
+1
+20
+7 5 5 9 17 9 15 10 13 11 13 10 18 7 17 13 8 7 9 13
+4
+1 4 8 12
+ */
+/*
+TEST.OUT
+13 -14
+30 -8
+78 13
+88 17
+
+100 20
+
 
 1 1
 2 2
@@ -192,36 +221,11 @@ public class Source {
 20 brak
 11 brak
 
----------------
 
-1
-20
-7 5 5 9 17 9 15 10 13 11 13 10 18 7 17 13 8 7 9 13
-4
-1 4 8 12
 
-+++
 
 1 5
 4 7
 8 9
 12 11
-
----------------
-
-1
-100
-0 57 48 38 28 39 5 69 4 89 58 23 83 72 78 59 88 19 100 55 43 35 16 29 43 49 3 51 0 25 39 23 18 86
-32 41 30 6 70 70 69 90 71 18 80 45 25 93 1 80 64 8 66 20 91 46 46 99 61 45 29 87 33 62 57 1 78 22
-26 4 88 52 36 48 67 12 9 69 49 40 58 71 10 30 48 91 70 47 77 66 67 22 75 84 63 15 48 14 16 83
-5
-3 10 40 74 98
-
-+++
-
-3 1
-10 8
-40 39
-74 69
-98 93
  */
